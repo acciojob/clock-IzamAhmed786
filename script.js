a651dad37e6f54a080beea8676404c2d9dfdab55
IzamAhmed786 <izamahmed786@gmail.com>
2025-09-28 10:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let date = new Date();
+
+let time=`${date.getMonth()}/${date.getDate()}/${date.getFullYear()},${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+setInterval(()=>{	document.getElementById("timer").innerText = time
+},1000)
+
