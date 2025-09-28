02e2589c1c7fa33b69291e1d5d6573095c2d9513
IzamAhmed786 <izamahmed786@gmail.com>
2025-09-28 11:11:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 let date = new Date();
+let ap=date.getHours()<12 ? "AM" : "PM"
 
-let time=`${date.getMonth()}/${date.getDate()}/${date.getFullYear()},${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+let time=`${date.getMonth()}/${date.getDate()}/${date.getFullYear()},${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}${ap}`;
 
 setInterval(()=>{	document.getElementById("timer").innerText = time
 },1000)
